51643405240d174a7fb7089a8a6c06171b8082f5
Unknown <parker.thurston22@gmail.com>
2018-09-07 10:31:27 -0600

    Jumpy Jump

--- 2D Game/Assets/Scripts/CharacterMove.cs
+++ 2D Game/Assets/Scripts/CharacterMove.cs
@@ -4,14 +4,49 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 
-	public int moveSpeed = 10;
+	//Player Movement Variables
+	public int MoveSpeed = 10;
+	public float JumpHeight;
+
+	//Player Grounded Variables
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
+
+	//Non-Stick Player
+	//private float moveVelocity;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
 	// Update is called once per frame
 	void Update () {
-		
+
+	//This code makes the character jump
+	if(Input.GetKeyDown (KeyCode.Space) && grounded) {
+		Jump();
+	}	
+
+	//Double jump code
+	/*
+	if (grounded)
+	dubleJump = false;
+
+	if (Input.GetKeyDown(KeyCode.Space)&& !doubleJpum&& !grounded){
+		Jump();
+		doubleJump = true;
 	}
+*/
+
+	}
+	public void Jump(){
+	GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,JumpHeight);
+    }
+
 }
