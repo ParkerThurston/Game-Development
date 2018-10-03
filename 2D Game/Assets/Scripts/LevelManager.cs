d035c353bae4586f02fb465e651cd46f06071370
Unknown <parker.thurston22@gmail.com>
2018-10-03 10:28:45 -0600

    Death surrounds us

---
+++ 2D Game/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject CurrentCheckPoint;
+	private Rigidbody2D PC;
+
+	//Particles
+	public GameObject DeathParticle;
+	public GameObject RespawnParticle;
+
+	//Respawn Delay
+	public float RespawnDelay;
+
+	//Point Penalty on Death
+	public int PointPenaltyOnDeath;
+
+	//Store Gravity Value
+	private float GravityStore;
+
+
+	// Use this for initialization
+	void Start () {
+	PC = FindObjectOfType<Rigidbody2D>();		
+	}
+	
+	public void RespawnPlayer(){
+		StartCoroutine("RespawnPlayerCo");
+
+	}
+
+	public IEnumerator RespawnPlayerCo(){
+		//Generate Death Particle
+		Instantiate (DeathParticle, PC.transform.position, PC.transform.rotation);
+		//Hide PC
+		//(Used to be) PC.enable = false;
+		PC.GetComponent<Renderer> ().enabled = false;
+		//Gravity Reset
+		GravityStore=PC.GetComponent<Rigidbody2D>().gravityScale;
+		PC.GetComponent<Rigidbody2D>().gravityScale = 0f;
+		PC.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		//Point Penalty
+		ScoreManager.AddPoints(-PointPenaltyOnDeath);
+		//Debug Message
+		Debug.Log("PC Respawn");
+		//Respawn Delay
+		yield return new WaitForSeconds (RespawnDelay);
+		//Gravity Restore
+		PC.GetComponent<Rigidbody2D>().gravityScale = GravityStore;
+		//Match PCs transform position
+		PC.transform.position = CurrentCheckPoint.transform.position;
+		//Show PC
+		//(Used to be)PC.enable = true;
+		PC.GetComponent<Renderer> ().enabled = true;
+		//Spawn Particle Effect
+		Instantiate (RespawnParticle, CurrentCheckPoint.transform.position, CurrentCheckPoint.transform.rotation);
+
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
