25a4d35096149a133591acbd6628b4c9e75db784
Unknown <parker.thurston22@gmail.com>
2019-01-07 10:17:33 -0700

    End Screen

---
+++ 2D Game/Assets/Scripts/WinBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinBox : MonoBehaviour {
+
+	 void OnTriggerEnter2D (Collider2D other) {
+		
+		if(other.name== "PC"){
+			SceneManager.LoadScene(2);
+		}
+	 }
+}
