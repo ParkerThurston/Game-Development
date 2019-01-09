2411c19a5c21dc90ca218e1ea4c5170cca50649f
Unknown <parker.thurston22@gmail.com>
2019-01-09 09:59:56 -0700

    Build

---
+++ 2D Game/Assets/Scripts/KennyLink.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KennyLink : MonoBehaviour {
+
+	public void OpenChannel(){
+
+		Application.OpenURL("https://opengameart.org/users/kenney");
+	}
+}
